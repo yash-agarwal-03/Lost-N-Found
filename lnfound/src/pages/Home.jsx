1db45fd08060babe78f7b940270a91b792272cef
Yash Agarwal <agarwalyash1174@gmail.com>
2025-04-13 12:41:53 +0530

    Frontend-Update-2: Basic alert created

--- lnfound/src/pages/Home.jsx
+++ lnfound/src/pages/Home.jsx
@@ -4,16 +4,25 @@ import FoundItemsList from '../components/FoundItemsList';
 import LinkButton from '../components/linkButton';
 const Home = () => {
  
-  const handleAddTicket = (ticket) => {
-    const newTicket = { _id: Date.now(), ...ticket };
-    const response=fetch("http://localhost:5000/lnf/addLostTicket", {
-      method:"POST",
-      headers:{
-        'Content-Type':'application/json'
-      },
-      body: JSON.stringify(newTicket)
+  const handleAddTicket = async (ticket) => {
+    const id=Date.now();
+    const newTicket = { _id: id, ...ticket };
+    try{
+      const response=await fetch("http://localhost:5000/lnf/addLostTicket", {
+        method:"POST",
+        headers:{
+          'Content-Type':'application/json'
+        },
+        body: JSON.stringify(newTicket)
+      }
+      )
+      alert(response.statusText==="OK"?`Ticket added successfully\nTicket ID : ${id}`:"Unsuccessful in adding ticket");
+
     }
-    )
+    catch(err){
+      alert("An error occurred while submitting\nPlease try again later");
+      console.error("Error in adding lost ticket:", err);
+    }
   };
 
   return (
